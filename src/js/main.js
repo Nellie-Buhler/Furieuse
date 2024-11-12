0fb771c3da564b5a5bc460ee2ee57891d144c325
nellliebuhler <122967704+Nellie-Buhler@users.noreply.github.com>
2024-11-12 15:04:41 +0100

    stylisation de l'écran de chargement

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import { gsap } from "gsap";
 
-//var loadingScreen = document.querySelector(".l-loadingScreen");
+var loadingScreen = document.querySelector(".l-loading-screen");
 
-// window.addEventListener("load", function () {
-//   loadingScreen.style.display = "none";
-// });
+window.addEventListener("load", function () {
+  loadingScreen.style.display = "none";
+});
