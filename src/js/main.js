2b84796278eae0a7378456d11aac9afa51918ec2
nellliebuhler <122967704+Nellie-Buhler@users.noreply.github.com>
2024-11-12 14:45:07 +0100

    création d'un squelette html

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 import { gsap } from "gsap";
+
+//var loadingScreen = document.querySelector(".l-loadingScreen");
+
+// window.addEventListener("load", function () {
+//   loadingScreen.style.display = "none";
+// });
