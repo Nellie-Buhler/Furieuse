b6a096453ba2b4baf6f54b9436ac362b5651d0db
nellliebuhler <122967704+Nellie-Buhler@users.noreply.github.com>
2024-11-13 18:22:56 +0100

    mise en page du footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// ------écran de chargement-------------------------------------------
 import { gsap } from "gsap";
 
 var loadingScreen = document.querySelector(".l-loading-screen");
@@ -5,3 +6,26 @@ var loadingScreen = document.querySelector(".l-loading-screen");
 window.addEventListener("load", function () {
   loadingScreen.style.display = "none";
 });
+
+// ------effet scroll-------------------------------------------------------------------
+
+const scrollCall = document.querySelector(".l-scroll-call");
+const footer = document.querySelector(".footer");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        scrollCall.style.position = "absolute";
+      } else {
+        scrollCall.style.position = "fixed";
+      }
+    });
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
+
+observer.observe(footer);
