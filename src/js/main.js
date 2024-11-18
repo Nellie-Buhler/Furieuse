6df50b936b4f3cdf37cf545f1b9734e8863da7cb
nellliebuhler <122967704+Nellie-Buhler@users.noreply.github.com>
2024-11-18 12:52:23 +0100

    ajout des animation d'apparition

--- src/js/main.js
+++ src/js/main.js
@@ -69,20 +69,178 @@ gsap.to("#bubble-family-paint-01", {
     start: "center bottom",
     endTrigger: "#bubble-family-paint-02",
     end: "top top",
-    // scrub: 1,
+    scrub: 1,
     markers: true,
   },
 });
 
 gsap.to("#bubble-family-paint-02", {
   opacity: 1,
-  x: "-250%",
+  x: "-50%",
   scrollTrigger: {
     trigger: "#bubble-family-paint-02",
     start: "center bottom",
     endTrigger: ".night-discusion",
     end: "top top",
-    // scrub: 1,
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-night-discusion-01", {
+  opacity: 1,
+  x: "150%",
+  scrollTrigger: {
+    trigger: "#bubble-night-discusion-01",
+    start: "center bottom",
+    endTrigger: "#bubble-night-discusion-02",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-night-discusion-02", {
+  opacity: 1,
+  x: "150%",
+  scrollTrigger: {
+    trigger: "#bubble-night-discusion-02",
+    start: "center bottom",
+    endTrigger: ".ysa-escape",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-ysa-escape-01", {
+  opacity: 1,
+  x: "150%",
+  scrollTrigger: {
+    trigger: "#bubble-ysa-escape-01",
+    start: "center bottom",
+    endTrigger: "#bubble-ysa-escape-02",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-ysa-escape-02", {
+  opacity: 1,
+  x: "-50%",
+  scrollTrigger: {
+    trigger: "#bubble-ysa-escape-02",
+    start: "center bottom",
+    endTrigger: ".alone-bubble",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-alone-bubble-01", {
+  opacity: 1,
+  x: "20%",
+  scrollTrigger: {
+    trigger: "#bubble-alone-bubble-01",
+    start: "center bottom",
+    endTrigger: "#bubble-alone-bubble-02",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-alone-bubble-02", {
+  opacity: 1,
+  x: "-20%",
+  scrollTrigger: {
+    trigger: "#bubble-alone-bubble-02",
+    start: "center bottom",
+    endTrigger: ".maxine-paint",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-maxine-paint-01", {
+  opacity: 1,
+  x: "150%",
+  scrollTrigger: {
+    trigger: "#bubble-maxine-paint-01",
+    start: "center bottom",
+    endTrigger: ".maxine-sketch",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#bubble-maxine-paint-02", {
+  opacity: 1,
+  y: "20%",
+  scrollTrigger: {
+    trigger: "#bubble-maxine-paint-02",
+    start: "center bottom",
+    endTrigger: ".maxine-sketch",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#ending-night-star-bubble", {
+  opacity: 1,
+  x: "150%",
+  scrollTrigger: {
+    trigger: "#ending-night-star-bubble",
+    start: "center bottom",
+    endTrigger: ".footer",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+// ------effet case-------------------------------------------------------------------
+
+gsap.to("#ending-comic-box-01", {
+  opacity: 1,
+  x: "50%",
+  scrollTrigger: {
+    trigger: "#ending-comic-box-01",
+    start: "center bottom",
+    endTrigger: "#ending-comic-box-02",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#ending-comic-box-02", {
+  opacity: 1,
+  x: "-50%",
+  scrollTrigger: {
+    trigger: "#ending-comic-box-02",
+    start: "center bottom",
+    endTrigger: "#ending-comic-box-03",
+    end: "top top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+gsap.to("#ending-comic-box-03", {
+  opacity: 1,
+  x: "50%",
+  scrollTrigger: {
+    trigger: "#ending-comic-box-03",
+    start: "center bottom",
+    endTrigger: ".ending-night-star",
+    end: "top top",
+    scrub: 1,
     markers: true,
   },
 });
